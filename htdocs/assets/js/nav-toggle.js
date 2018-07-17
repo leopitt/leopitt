c497b93f46c6b134cfd678640483ce15a67c495f
Leo Pitt <leopitt@function-design.co.uk>
2018-07-17 13:07:56 +0100

    Toggle button animation using GSAP. Code standards.

--- htdocs/assets/js/nav-toggle.js
+++ htdocs/assets/js/nav-toggle.js
@@ -5,11 +5,34 @@
 
 (function($) {
 
+  function animateToggleIn() {
+    // Reset positions
+    TweenMax.set('.bar:nth-child(2)', {scaleX: 1});
+    // Animate
+    TweenMax.to('.bar:nth-child(1)', .125, {y: 7, rotation: '+=45', ease:Power1.easeOut});
+    TweenMax.to('.bar:nth-child(2)', .125, {scaleX: 0.20689655172414, ease:Power1.easeOut});
+    TweenMax.to('.bar:nth-child(3)', .125, {y: -7, rotation: '+=45', ease:Power1.easeOut});
+  }
+
+  function animateToggleOut() {
+    TweenMax.to('.bar:nth-child(1)', .125, {y: 0, rotation: '+=45', ease:Power2.easeOut});
+    TweenMax.to('.bar:nth-child(2)', .125, {scaleX: 1, ease:Power2.easeOut});
+    TweenMax.to('.bar:nth-child(3)', .125, {y: 0, rotation: '+=45', ease:Power2.easeOut});
+  }
+
   $(document).ready(function () {
 
     $('.nav-toggle').bind('click', function() {
-      $('.nav-main').toggleClass('visible');
-      $(this).toggleClass('active');
+      if (!$(this).hasClass('active')) {
+        $('.nav-main').toggleClass('visible');
+        $(this).toggleClass('active');
+        animateToggleIn();
+      }
+      else {
+        $('.nav-main').removeClass('visible');
+        $(this).removeClass('active');
+        animateToggleOut();
+      }
     });
 
   });
