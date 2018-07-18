cbf8ef327e1e80c4743b6adf632cf1f5ae58cb72
Leo Pitt <leopitt@function-design.co.uk>
2018-07-18 12:53:52 +0100

    Unsuccessful attempt at applying clip-path dynamically.

--- htdocs/assets/js/animate.js
+++ htdocs/assets/js/animate.js
@@ -5,22 +5,25 @@
 
 (function($) {
 
+  var animateItemClass = 'nav-animate-item';
   var elementColor = '#bd1857';
   var numCols = 20;
   var numRows = 10;
   var numElements = numCols * numRows;
-  var canvasClass = '.animate-canvas'
+  var canvasClass = '#nav-clip'
+  var refClass = 'body';
   var delayBetweenAnimations = .001;
 
   function build() {
     var canvas = $(canvasClass);
-    var elementHeight = canvas.height() / numRows;
-    var elementWidth = canvas.width() / numCols;
+    var ref = $(refClass);
+    var elementHeight = ref.height() / numRows;
+    var elementWidth = ref.width() / numCols;
     var x = 0;
     var y = 0;
 
     // Delete any existing elements.
-    $('.animate-item').remove();
+    $('.' + animateItemClass).remove();
 
     // Create elements.
     for (var i = 0; i < numElements; i++) {
@@ -28,17 +31,8 @@
       row = i % numRows;
       col = Math.floor(i / numRows);
       // Create element.
-      var animateItem = $('<div></div>').addClass('animate-item').addClass('animate-item-' + i).css({
-        'display': 'block',
-        'background-color': elementColor,
-        'height': elementHeight + 'px',
-        'width': elementWidth + 'px',
-        'position': 'absolute',
-        'z-index': -1,
-        'top': (elementHeight * row) + 'px',
-        'left': (elementWidth * col) + 'px',
-      });
-      TweenMax.set(animateItem, {scaleY: 0, rotation: 45, transformOrigin: '50% 50%'});
+      var animateItem = $('<rect x="' + (elementWidth * col) + '" y="' + (elementHeight * row) + '" width="' + elementWidth + '" height="' + elementHeight + '" />').addClass('bg-highlight').addClass(animateItemClass).addClass(animateItemClass + '-' + i);
+      //TweenMax.set(animateItem, {scaleY: 0, rotation: 45, transformOrigin: '50% 50%'});
       canvas.append(animateItem);
       console.log('Loop end.');
     };
@@ -47,7 +41,7 @@
   function animate() {
     // Animate elements.
     for (var i = 0; i < numElements; i++) {
-      var animateItem = $('.animate-item-' + i);
+      var animateItem = $('.' + animateItemClass + '-' + i);
       TweenMax.to(animateItem, 1, {scaleY: 1, rotation: 0, ease:Power4.easeOut, delay: (delayBetweenAnimations * i)});
     }
   }
