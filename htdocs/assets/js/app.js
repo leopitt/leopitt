24660d25e3a1cf44a88b8418660307bf3c29ef8d
Leo Pitt <leopitt@function-design.co.uk>
2018-07-19 12:50:11 +0100

    Added moment.js

--- htdocs/assets/js/app.js
+++ htdocs/assets/js/app.js
@@ -1,26 +1,37 @@
-var app = new Vue({
+apiURL = 'http://leopittapi.docksal/api/posts';
+
+new Vue({
   el: '#app',
+
   data: {
     homeTitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    posts: [
-      {
-        title: 'Phasellus lorem orci, sollicitudin quis tortor et',
-        teaser: 'Nunc feugiat vel dolor ut tristique. Nulla sagittis sem sed suscipit semper. Mauris non ligula in elit semper porttitor quis sit amet neque. Phasellus lorem orci, sollicitudin quis tortor et, viverra tempus felis.',
-        date: 'Monday, 10th July 2018',
-        numComments: 3,
-      },
-      {
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
-        teaser: 'Feugiat vel dolor ut tristique. Nulla sagittis sem sed suscipit semper. Mauris non ligula in elit semper porttitor quis sit amet neque. Phasellus lorem orci, sollicitudin quis tortor et, viverra tempus felis.',
-        date: 'Monday, 9th July 2018',
-        numComments: 5,
-      },
-      {
-        title: 'Feugiat vel dolor ut tristique',
-        teaser: 'Feugiat vel dolor ut tristique. Nulla sagittis sem sed suscipit semper. Mauris non ligula in elit semper porttitor quis sit amet neque. Phasellus lorem orci, sollicitudin quis tortor et, viverra tempus felis.',
-        date: 'Monday, 21st June 2018',
-        numComments: 26,
-      },
-    ],
+    posts: []
+  },
+
+  mounted: function() {
+    this.getPosts();
+  },
+
+  methods: {
+    getPosts: function(){
+
+      console.log('getPosts');
+
+      this.$http.get(apiURL).then(response => {
+        this.posts = response.body;
+        console.log(this.posts);
+      }, response => {
+        console.log('oops');
+      });
+    }
+  },
+
+  filters: {
+    formatDate: function(value) {
+      if (value) {
+        return moment(String(value)).format('MMMM Do YYYY')
+      }
+    }
   }
+
 });
