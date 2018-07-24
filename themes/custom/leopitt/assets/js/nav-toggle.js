45396650b5bc7ebb4c509c072eb3055ad1b47c77
Leo Pitt <leopitt@function-design.co.uk>
2018-07-24 18:50:46 +0100

    Various responsiveness.

--- themes/custom/leopitt/assets/js/nav-toggle.js
+++ themes/custom/leopitt/assets/js/nav-toggle.js
@@ -14,9 +14,16 @@
         // Reset positions
         TweenMax.set('.bar:nth-child(2)', {scaleX: 1});
         // Animate
-        TweenMax.to('.bar:nth-child(1)', .125, {y: 7, rotation: '+=45', ease:Power1.easeOut});
-        TweenMax.to('.bar:nth-child(2)', .125, {scaleX: 0.20689655172414, ease:Power1.easeOut});
-        TweenMax.to('.bar:nth-child(3)', .125, {y: -7, rotation: '+=45', ease:Power1.easeOut});
+        if ($(window).width() < 640) {
+          TweenMax.to('.bar:nth-child(1)', .125, {y: 3, rotation: '+=45', ease:Power1.easeOut});
+          TweenMax.to('.bar:nth-child(2)', .125, {scaleX: 0.20689655172414, ease:Power1.easeOut});
+          TweenMax.to('.bar:nth-child(3)', .125, {y: -3, rotation: '+=45', ease:Power1.easeOut});
+        }
+        else {
+          TweenMax.to('.bar:nth-child(1)', .125, {y: 7, rotation: '+=45', ease:Power1.easeOut});
+          TweenMax.to('.bar:nth-child(2)', .125, {scaleX: 0.20689655172414, ease:Power1.easeOut});
+          TweenMax.to('.bar:nth-child(3)', .125, {y: -7, rotation: '+=45', ease:Power1.easeOut});
+        }
       }
 
       function animateToggleOut() {
@@ -26,8 +33,14 @@
       }
 
       function animateMaskIn() {
-        var maskStart = [0,0,9.89,0,16.51,100,0,100];
-        var maskEnd = [0,0,110,0,116,100,0,100];
+        if ($(window).width() < 640) {
+          var maskStart = [0,0,2,0,2,100,0,100];
+          var maskEnd = [0,0,100,0,100,100,0,100];
+        }
+        else {
+          var maskStart = [0,0,9.89,0,16.51,100,0,100];
+          var maskEnd = [0,0,110,0,116,100,0,100];
+        }
         maskEnd.onUpdate = function() {
           TweenMax.set('.nav-main', {webkitClipPath:'polygon(' + maskStart[0] + '% ' + maskStart[1] + '%, ' + maskStart[2] + '% ' + maskStart[3] + '%, ' + maskStart[4] + '% ' + maskStart[5] + '%, ' + maskStart[6] + '% ' + maskStart[7] + '%)'});
         };
@@ -36,8 +49,14 @@
       }
 
       function animateMaskOut() {
-        var maskStart = [0,0,9.89,0,16.51,100,0,100];
-        var maskEnd = [0,0,110,0,116,100,0,100];
+        if ($(window).width() < 640) {
+          var maskStart = [0,0,2,0,2,100,0,100];
+          var maskEnd = [0,0,100,0,100,100,0,100];
+        }
+        else {
+          var maskStart = [0,0,9.89,0,16.51,100,0,100];
+          var maskEnd = [0,0,110,0,116,100,0,100];
+        }
         maskStart.onUpdate = function() {
           TweenMax.set('.nav-main', {webkitClipPath:'polygon(' + maskEnd[0] + '% ' + maskEnd[1] + '%, ' + maskEnd[2] + '% ' + maskEnd[3] + '%, ' + maskEnd[4] + '% ' + maskEnd[5] + '%, ' + maskEnd[6] + '% ' + maskEnd[7] + '%)'});
         };
