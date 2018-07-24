c74c9f4096eae55eda046eb472b7137ac6d116a1
Leo Pitt <leopitt@function-design.co.uk>
2018-07-24 16:51:29 +0100

    Dev settings.

--- settings.local.php
+++ settings.local.php
@@ -3,8 +3,11 @@
 /**
  * Enable local development services.
  */
+
+/* Load development services. */
 $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
 
+/* Local db settings. */
 $databases['default']['default'] = array (
   'database' => 'default',
   'username' => 'user',
@@ -15,12 +18,23 @@ $databases['default']['default'] = array (
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
 );
+
+/* Profile. */
 $settings['install_profile'] = 'standard';
+
+/* Config. */
 $config_directories['sync'] = 'sites/leopitt.co.uk/files/config_2NKT5vPjA5_Nl_kpledicwKiVOLjkTJa_VRZj-69ViKNzIF6X6CltQk5rApBPB1ZNkEytlgt8w/sync';
 
+/* Trusted host. */
 $settings['trusted_host_patterns'] = array(
   '^docksal$',
   '^.+\.leopitt\.co\.uk$',
   '^leopitt\.co\.uk$',
   '^.+\.docksal$',
 );
+
+/* Caching and performance. */
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
