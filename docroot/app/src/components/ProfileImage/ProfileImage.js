966dcfb12798a7ecfae7c1bebefccf3efcddcbc6
Leo Pitt <leopitt@function-design.co.uk>
2022-10-07 12:16:54 +0100

    Added profile image.

--- docroot/app/src/components/ProfileImage/ProfileImage.js
+++ docroot/app/src/components/ProfileImage/ProfileImage.js
@@ -5,7 +5,12 @@ class ProfileImage extends React.Component {
   render() {
     return <div className={"c-profile-image"}>
       <div className={"c-profile-image__inner"}>
-        <img className={"c-profile-image__image"} src={"/profile.jpg"} />
+        <picture className={"c-profile-image__image"}>
+          <source media={"(min-width: 1028px)"} srcSet={"/leo-pitt-md.jpg"} />
+          <source media={"(min-width: 375px)"} srcSet={"/leo-pitt-sm.jpg"} />
+          <source srcSet={"/leo-pitt-xs.jpg"} />
+          <img src={"/leo-pitt-md.jpg"} alt={"Leo Pitt"} />
+        </picture>
       </div>
     </div>;
   }
